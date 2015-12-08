c52b318777b1c3ed3dfacd00ed1d0d8fd3a7a306
Maxim Pervushin <maxim.pervushin@gmail.com>
2015-12-08 16:21:45 +0300

    PresentationResearch subproject added

---
+++ PresentationResearch/PresentationResearch/FirstViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FirstViewController.swift
+//  PresentationResearch
+//
+//  Created by Maxim Pervushin on 08/12/15.
+//  Copyright © 2015 Maxim Pervushin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+}
+
