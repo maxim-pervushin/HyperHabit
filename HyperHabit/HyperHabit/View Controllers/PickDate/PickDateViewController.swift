eb24c64077a9051bbce9645146bd2de24195ecfa
Maxim Pervushin <maxim.pervushin@gmail.com>
2015-12-12 19:59:10 +0300

    PickDateViewController presentation style changed

--- HyperHabit/HyperHabit/View Controllers/PickDate/PickDateViewController.swift
+++ HyperHabit/HyperHabit/View Controllers/PickDate/PickDateViewController.swift
@@ -16,6 +16,14 @@ class PickDateViewController: UIViewController {
 
     // MARK: DatePickerViewController @IB
 
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var headerTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var headerHeightConstraint: NSLayoutConstraint!
+
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var containerTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var containerHeightConstraint: NSLayoutConstraint!
+
     @IBOutlet private weak var datePicker: UIDatePicker!
 
     @IBAction func pickButtonAction(sender: AnyObject) {
@@ -52,7 +60,7 @@ class PickDateViewController: UIViewController {
 
         if let date = date {
             datePicker.date = date
-        } else  {
+        } else {
             datePicker.date = NSDate()
         }
 
@@ -61,5 +69,130 @@ class PickDateViewController: UIViewController {
         } else {
             datePicker.maximumDate = nil
         }
+    }
+
+    private func commonInit() {
+        transitioningDelegate = self
+        modalPresentationStyle = .Custom
+    }
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        commonInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+}
+
+extension PickDateViewController {
+
+    private func prepareForPresentation() {
+        view.bringSubviewToFront(headerView)
+        headerTopConstraint.constant = -headerHeightConstraint.constant
+        containerTopConstraint.constant = -headerHeightConstraint.constant - containerHeightConstraint.constant
+        view.layoutIfNeeded()
+    }
+
+    private func performPresentationWithDuration(duration: NSTimeInterval, completion: ((Bool) -> Void)?) {
+        view.layoutIfNeeded()
+        headerTopConstraint.constant = 0
+        view.setNeedsUpdateConstraints()
+        UIView.animateWithDuration(duration / 2, delay: 0, usingSpringWithDamping: 0.85, initialSpringVelocity: 0.5, options: [.CurveEaseInOut, .TransitionNone], animations: {
+            // Layout header
+            self.view.layoutIfNeeded()
+        }, completion: {
+            _ in
+            self.containerTopConstraint.constant = self.headerHeightConstraint.constant
+            self.view.setNeedsUpdateConstraints()
+
+            UIView.animateWithDuration(duration / 2, delay: 0, usingSpringWithDamping: 0.85, initialSpringVelocity: 0.5, options: [.CurveEaseInOut, .TransitionNone], animations: {
+                // Layout content
+                self.view.layoutIfNeeded()
+            }, completion: completion)
+        })
+    }
+
+    private func prepareForDismissal() {
+        view.bringSubviewToFront(headerView)
+        headerTopConstraint.constant = 0
+        containerTopConstraint.constant = headerHeightConstraint.constant
+        view.layoutIfNeeded()
+    }
+
+    private func performDismissalWithDuration(duration: NSTimeInterval, completion: ((Bool) -> Void)?) {
+        view.layoutIfNeeded()
+        containerTopConstraint.constant = -headerHeightConstraint.constant - containerHeightConstraint.constant
+        view.setNeedsUpdateConstraints()
+        UIView.animateWithDuration(duration / 2, delay: 0, usingSpringWithDamping: 0.85, initialSpringVelocity: 0.5, options: [.CurveEaseInOut, .TransitionNone], animations: {
+            // Layout content
+            self.view.layoutIfNeeded()
+        }, completion: {
+            _ in
+            self.headerTopConstraint.constant = -self.headerHeightConstraint.constant
+            self.view.setNeedsUpdateConstraints()
+
+            UIView.animateWithDuration(duration / 2, delay: 0, usingSpringWithDamping: 0.85, initialSpringVelocity: 0.5, options: [.CurveEaseInOut, .TransitionNone], animations: {
+                // Layout header
+                self.view.layoutIfNeeded()
+            }, completion: completion)
+        })
+    }
+}
+
+extension PickDateViewController: UIViewControllerTransitioningDelegate {
+
+    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return self
+    }
+
+    func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return self
+    }
+}
+
+extension PickDateViewController: UIViewControllerAnimatedTransitioning {
+
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
+        return 0.5
+    }
+
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+
+        guard let fromViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey) else {
+            print("Invalid fromViewController")
+            return
+        }
+        guard let toViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey) else {
+            print("Invalid toViewController")
+            return
+        }
+        guard let containerView = transitionContext.containerView() else {
+            print("Invalid containerView")
+            return
+        }
+
+        if let pickDateViewController = toViewController as? PickDateViewController {
+            pickDateViewController.prepareForPresentation()
+            containerView.insertSubview(toViewController.view, belowSubview: fromViewController.view)
+            pickDateViewController.performPresentationWithDuration(transitionDuration(transitionContext), completion: {
+                _ in
+                transitionContext.completeTransition(!transitionContext.transitionWasCancelled())
+            })
+        }
+
+        if let pickDateViewController = fromViewController as? PickDateViewController {
+            pickDateViewController.prepareForDismissal()
+            pickDateViewController.performDismissalWithDuration(transitionDuration(transitionContext), completion: {
+                _ in
+                fromViewController.view.removeFromSuperview()
+                transitionContext.completeTransition(!transitionContext.transitionWasCancelled())
+            })
+        }
+    }
+
+    func animationEnded(transitionCompleted: Bool) {
     }
 }
